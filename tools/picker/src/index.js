b3735c8a09467a3bebefc892d96aea1e933279fc
ehrro <dejan.eric@outlook.com>
2024-10-11 16:33:19 +0200

    Test

--- tools/picker/src/index.js
+++ tools/picker/src/index.js
@@ -11,9 +11,9 @@ import './styles.css';
  * Object containing all configuration files that should be exposed in the picker.
  */
 const configFiles = {
-    'prod': 'https://main--aem-boilerplate-commerce--hlxsites.hlx.live/configs.json?sheet=prod',
-    'stage': 'https://main--aem-boilerplate-commerce--hlxsites.hlx.live/configs-stage.json',
-    'dev': 'https://main--aem-boilerplate-commerce--hlxsites.hlx.live/configs-dev.json',
+    'prod': 'https://main--gum--sunstar-global.aem.live/configs.json?sheet=prod',
+    'stage': 'https://main--gum--sunstar-global.aem.live/configs-stage.json',
+    'dev': 'https://main--gum--sunstar-global.aem.live/configs-dev.json',
 }
 /**
  * Default configuration to be loaded.
